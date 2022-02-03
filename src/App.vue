d4997048bf49867594fbd7bf7fa7256f54597285
Steven Rocha <67056904+RochaMoran@users.noreply.github.com>
2022-02-02 20:09:17 -0600

    ADD: practice vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,6 @@
 <template>
   <div id="app" class="container">
-    <Header />
-    <List />
-    <Body />
+   <router-link to="/">Home</router-link>
+   <router-view />
   </div>
 </template>
-
-<script>
-import Header from './components/Header.vue';
-import List from './components/List.vue';
-import Body from './components/Body.vue';
-
-export default {
-  name: 'App',
-  components: {
-    Header,
-    List,
-    Body
-  }
-}
-</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container">
+        <h1>Home</h1>
+        <Header />
+        <List />
+        <Body />
+    </div>
+</template>
+
+<script>
+import Header from '../components/Header.vue'
+import Body from '../components/Body.vue'
+import List from '../components/List.vue'
+
+export default {
+    name: 'Home',
+    components: {
+        Header,
+        Body,
+        List
+    }
+}
+</script>
